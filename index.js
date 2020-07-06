d5715ecde0597c563009343e3218a96de2b89a58
manzit-sai <51959515+manzit-sai@users.noreply.github.com>
2020-07-06 14:16:07 +0530

    Add initial CV changes

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var hell="Hello coders";
+console.log(hell);
+
+document.getElementById("text").innerText='Good evening'
+
+var god=prompt("username")+prompt("passewew");
+document.getElementById("text").innerHTML=god;
+
+
+
